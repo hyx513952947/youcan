fde8ad02dc9102f83213ddf5a2955dec12c0b627
hyx513952947 <513952947@qq.com>
2018-08-03 14:05:08 +0800

    增加适配的资源值文件

--- app/src/main/java/top/huangguaniu/youcan/ui/main/MainActivity.java
+++ app/src/main/java/top/huangguaniu/youcan/ui/main/MainActivity.java
@@ -1,29 +1,21 @@
 package top.huangguaniu.youcan.ui.main;
 
-import android.Manifest;
-import android.support.annotation.NonNull;
 import android.view.View;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.OnClick;
-import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.OnShowRationale;
-import permissions.dispatcher.PermissionRequest;
-import permissions.dispatcher.RuntimePermissions;
 import top.huangguaniu.youcan.R;
 import top.huangguaniu.youcan.ui.BaseActivity;
 import top.huangguaniu.youcan.ui.Layout;
 import top.huangguaniu.youcan.ui.main.views.DragRightFrameLayout;
-import top.huangguaniu.youcan.ui.main.views.Logger;
 import top.huangguaniu.youcan.ui.main.views.MenuButton;
 import top.huangguaniu.youcan.ui.main.views.MenuLayout;
 
 /**
  * @author hyx
  */
-@RuntimePermissions
 @Layout(layout = R.layout.main_activity)
 public class MainActivity extends BaseActivity implements MenuLayout.OnMenuItemClickListener
         ,MainActivityContract.View {
@@ -41,7 +33,6 @@ public class MainActivity extends BaseActivity implements MenuLayout.OnMenuItemC
 
     @Inject
     protected void injectPresenter(MainActivityContract.Presenter presenter) {
-        Logger.i("啊哈哈的好哇"+presenter.toString());
     }
 
     @Override
@@ -49,25 +40,9 @@ public class MainActivity extends BaseActivity implements MenuLayout.OnMenuItemC
         layoutDrag.closeMenu();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
-            grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
     @OnClick(R.id.button_menu_closed)
     public void onViewClicked() {
         layoutDrag.closeMenu();
-    }
-
-    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
-    void requestPermission() {
-
-    }
-
-    @OnShowRationale({Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
-    void onPermissionResult(final PermissionRequest request) {
-        Logger.i("结果："+request.toString());
     }
 
 }
